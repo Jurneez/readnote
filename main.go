dd0ce64fcb00eeb2cd2dd50e7c24bfc735502f3c
zhumengdi <jurnee.zhu@ucloud.cn>
2022-07-12 17:49:51 +0800

    http helloworld

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
+import (
+	"net/http"
+	"tool/readnote/config"
+)
+
 func init() {
-	// config.LoadConfig("./config/config.toml")
+	config.LoadConfig("./config/config.toml")
 
 	// mysqlTest := config.Conf.Mysql["test"]
 	// testDB := mysql.New(mysqlTest.Host, mysqlTest.Port, mysqlTest.UserName, mysqlTest.Password, "test")
 	// testDB.Execx("./sql/file.sql")
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("login"))
+}
 func main() {
+	http.HandleFunc("/login", login)
+	http.ListenAndServe(":9091", nil)
 }
